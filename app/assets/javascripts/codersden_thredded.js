011b012bff2daf3cb18ebb7c757b65abbf9f9df9
Ricardo Canales <canalesb93@gmail.com>
2017-04-17 21:13:11 -0500

    Adds reply to message

--- app/assets/javascripts/codersden_thredded.js
+++ app/assets/javascripts/codersden_thredded.js
@@ -1,2 +1,15 @@
 //= require locales/jquery.timeago.es
 //= require thredded
+
+$( ".thredded--post--reply" ).click(function( event ) {
+  event.preventDefault();
+
+  // Name Query
+  var nameElement = $(this).prevAll("header").children(".thredded--post--user").children("a")[0];
+
+  var reply = "@" + nameElement.text + " " + $("#post_content").val();
+
+  $("#post_content").focus().val(reply);
+  $("#post_content").change()
+
+});
